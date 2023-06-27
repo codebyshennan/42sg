fe227e4ff932610e840b004ffc710b08e171e95d
Shen Nan Wong <shwong@c2r3s6.42singapore.sg>
2023-06-27 13:39:13 +0800

    add rush00

---
+++ rush00/rush03.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush00.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mramlee <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/25 20:47:45 by mramlee           #+#    #+#             */
+/*   Updated: 2023/06/27 12:21:56 by shwong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+void	ft_putchar(char a);
+
+void	print_x_or_y(int row_count, char a, char b, char y)
+{
+	if ((row_count == 1) || (row_count == y))
+	{
+		ft_putchar(x);
+	}
+	else
+	{
+		ft_putchar(y);
+	}
+}
+
+void	print_for_x_more_than_1(int x)
+{
+	if (x > 1)
+	{
+		print_x_or_y(row_count, 'C', 'B', y);
+	}
+}
+
+void	run_valid_condition(void)
+{
+	int	row_count;
+	int	column_count;
+
+	row_count = 1;
+	column_count = 1;
+	while (row_count <= y)
+	{
+		if ((row_count == 1) || (row_count == y))
+		{
+			ft_putchar('A');
+		}
+		else
+		{
+			ft_putchar('B');
+			while (column_count <= x - 2)
+			{
+				print_x_or_y(row_count, 'B', ' ', y);
+				column_count++;
+			}
+			column_count = 1;
+			print_for_x_more_than_1(x);
+			ft_putchar('\n');
+			row_count++;
+		}
+	}
+}
+
+void	rush(int x, int y)
+{
+	if (y <= 0 || x <= 0)
+	{
+		ft_putchar('!');
+		ft_putchar('\n');
+	}
+	else
+	{
+		run_valid_condition();
+	}
+}
